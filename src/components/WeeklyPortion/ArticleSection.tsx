a9e07ced72ba36831cb8981e01e26f3694a6fc0b
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-07-25 22:32:51 +0530

    fix: Fixed Header and Home page to follow Figma Design files

--- src/components/WeeklyPortion/ArticleSection.tsx
+++ src/components/WeeklyPortion/ArticleSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './styles.css'; // Ensure this import is correct
 import { Article } from './types'; // Import the Article interface
+import Image from 'next/image';
 
 // Define the props for the ArticleSection component
 interface ArticleSectionProps {
@@ -15,14 +15,14 @@ const ArticleSection: React.FC<ArticleSectionProps> = ({ heading, articles, togg
     <div className="article-section">
       <div className="flex justify-between items-center mb-4 section-header">
         <h2 className="text-xl text-black font-bold">{heading}</h2>
-        <button onClick={toggleSectionVisibility} className="button-default">
+        <button onClick={toggleSectionVisibility} className="text-red-700 font-bold underline">
           {isExpanded ? "Show Less" : "Show All"}
         </button>
       </div>
       <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 section-content`}>
         {articles.slice(0, isExpanded ? articles.length : 3).map((article, idx) => (
           <div key={idx} className="article-container">
-            <img src={article.image} alt={article.title} className="w-full h-48 object-cover" />
+            <Image src={article.image} alt={article.title} className="w-full aspect-video rounded-md" width={500} height={500} />
             <h3 className="text-lg font-semibold mt-4 text-articleText">{article.title}</h3>
             <p className="text-gray-600 mt-2">{article.description}</p>
           </div>
--- src/components/WeeklyPortion/weeklyPortion.tsx
+++ src/components/WeeklyPortion/weeklyPortion.tsx
@@ -1,14 +1,15 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import './styles.css'; // Ensure this import is correct
-import ArticleSection from './ArticleSection'; // Ensure this import path is correct
-import { Article, DaySections } from './types'; // Import the interfaces
+import React, { useEffect, useState } from "react";
+import ArticleSection from "./ArticleSection"; // Ensure this import path is correct
+import { Article, DaySections } from "./types"; // Import the interfaces
 
 // Function to fetch mock articles
 const fetchArticles = async (): Promise<DaySections> => {
   const mockArticle: Article = {
-    title: "International Yoga Day Celebrated with Enthusiasm at Gymnasium Yoga Centre, Strengthening Community Spirit",
-    description: "In celebration of International Yoga Day on 21st of June, a well-attended yoga event was observed at the Gymnasium Yoga Centre. The event began...",
+    title:
+      "International Yoga Day Celebrated with Enthusiasm at Gymnasium Yoga Centre, Strengthening Community Spirit",
+    description:
+      "In celebration of International Yoga Day on 21st of June, a well-attended yoga event was observed at the Gymnasium Yoga Centre. The event began...",
     image: "https://picsum.photos/400/300",
   };
   //only for mock demonstration
@@ -20,17 +21,19 @@ const fetchArticles = async (): Promise<DaySections> => {
   return {
     Section1: [{ heading: "Funny Friday", articles: articles.slice(0, 5) }],
     Section2: [{ heading: "Open Source", articles: articles.slice(3, 6) }],
-    Wednesday: [{ heading: "Heritage Highligths", articles: articles.slice(6, 9) }],
-    Thursday: [
-      { heading: "Tech Byte", articles: articles.slice(9, 12) },
-    ]
+    Wednesday: [
+      { heading: "Heritage Highligths", articles: articles.slice(6, 9) },
+    ],
+    Thursday: [{ heading: "Tech Byte", articles: articles.slice(9, 12) }],
   };
 };
 
 // Main React component
 const WeeklyPortion: React.FC = () => {
   const [sections, setSections] = useState<DaySections>({});
-  const [expandedSections, setExpandedSections] = useState<{ [key: string]: boolean }>({});
+  const [expandedSections, setExpandedSections] = useState<{
+    [key: string]: boolean;
+  }>({});
   const [showAllSections, setShowAllSections] = useState<boolean>(false); // State to control visibility of sections
 
   // useEffect hook to fetch articles on component mount
@@ -61,7 +64,7 @@ const WeeklyPortion: React.FC = () => {
     : Object.entries(sections).slice(0, 2); // Show only first two sections initially
 
   return (
-    <div className="container mx-auto min-h-screen flex flex-col p-4 bg-white">
+    <div className="">
       {/* Render sections */}
       {visibleSections.map(([day, daySections]) => (
         <div key={day} className="day-section mb-8">
@@ -70,7 +73,9 @@ const WeeklyPortion: React.FC = () => {
               key={idx}
               heading={section.heading}
               articles={section.articles}
-              toggleSectionVisibility={() => toggleSectionVisibility(`${day}-${idx}`)}
+              toggleSectionVisibility={() =>
+                toggleSectionVisibility(`${day}-${idx}`)
+              }
               isExpanded={expandedSections[`${day}-${idx}`]}
             />
           ))}
@@ -79,7 +84,10 @@ const WeeklyPortion: React.FC = () => {
 
       {/* Button to toggle visibility of all sections */}
       <div className="flex justify-center mb-8">
-        <button onClick={toggleAllSectionsVisibility} className="button-default">
+        <button
+          onClick={toggleAllSectionsVisibility}
+          className="text-red-700 font-bold underline"
+        >
           {showAllSections ? "Show Less Sections" : "Show More Sections"}
         </button>
       </div>
