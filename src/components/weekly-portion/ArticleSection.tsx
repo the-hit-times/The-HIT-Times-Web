cf64a7a9a4db0760ae898660811d08fc59338571
Kingshuk Hazra <kingshukhazra5@gmail.com>
2024-10-07 06:56:51 +0530

    Here i fixed some bugs and made some ui improvements

--- src/components/weekly-portion/ArticleSection.tsx
+++ src/components/weekly-portion/ArticleSection.tsx
@@ -24,16 +24,20 @@ const ArticleSection: React.FC<ArticleSectionProps> = ({
 }) => {
   return (
     <div className="article-section">
-      <div className="flex justify-between items-center mb-4 section-header">
-        <h2 className={poppins.className + " text-xl text-black font-bold"}>
+      <div className="flex justify-between items-center mb-4 section-header  rounded-xl scroll-smooth">
+        <h2 className={poppins.className + " text-xl text-black font-bold animate-fade-right animate-once animate-duration-500 animate-delay-500"}>
           {heading}
         </h2>
-        <Link
-          href={showAllLink ?? ""}
-          className={poppins.className + " text-red-700 font-bold underline"}
-        >
-          Show All
-        </Link>
+        <span className="relative flex  ">
+          <span className="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-red-400 opacity-75 "></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>     
+            <Link
+              href={showAllLink ?? ""}
+              className={poppins.className + " text-red-700 font-bold bg-red-200 rounded-md"}
+            >
+              Show All
+            </Link>
+        </span>
       </div>
       <div
         className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 section-content`}
