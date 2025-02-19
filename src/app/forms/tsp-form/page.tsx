bfed93eb11ca88df7174f1e49de48525c1a4c590
Kr1shnendu <majikrishnendu1@gmail.com>
2025-02-19 23:00:37 +0530

    feat: Unique email for TSP form submission

--- src/app/forms/tsp-form/page.tsx
+++ src/app/forms/tsp-form/page.tsx
@@ -118,16 +118,16 @@ export default function TSPForm() {
         body: JSON.stringify(formData),
       });
 
+      const data: any = await response.json();
+
       if (response.status != 201) {
-        toast.error("Something went wrong");
+        toast.error(data.msg || "Something went wrong");
         throw new Error(`HTTP error! status: ${response.status}`);
       } else {
         toast.success("Submitted successfully")
       }
-
-      const data: any = await response.json();
-      console.log(data);
       return true;
+
     } catch (error) {
       setIsSubmitted(false)
       toast.error("Try submitting again");
